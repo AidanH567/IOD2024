55e2592b0f445f798e116de8e44a13ff0f6efa8d
AidanH567 <herstikaidan@gmail.com>
2024-10-02 19:13:53 +1000

    edits

---
+++ Module_5/Routes/calculatorRoutes.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const app = express.Router();
+app1.get("/add", (req, res) => {
+    console.log("PARAMS", req.query);
+    res.send(req.query)
+  });
+
+  mpdule.exports = router;
--- Module_5/index.js
+++ Module_5/index.js
@@ -4,7 +4,16 @@ const port = 3000;
 const app1 = express();
 const port1 = 80;
 const testRoutes = require("./routes/myTestRoutes");
+const calculatorRoutes = require("./Routes/calculatorRoutes"); // For `routes` folder in the current directory.
+
+
+
+
 app.use("/mytest", testRoutes);
+
+//Routes
+
+app.use("/calculator", calculatorRoutes);
 
 app.listen(port, () => {
   console.log(`Example app listening
