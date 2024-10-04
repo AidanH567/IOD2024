624d9223788e1475b8269dd6a30a3c3d4bafcb60
AidanH567 <herstikaidan@gmail.com>
2024-10-04 10:22:11 +1000

    edits

--- Module_5/index.js
+++ Module_5/index.js
@@ -2,17 +2,13 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-
 const testRoutes = require("./Routes/testRoutes");
 const calculatorRoutes = require("./Routes/calculatorRoutes");
 
-
 app.use("/", express.static("public"));
-
 
 app.use("/mytest", testRoutes);
 app.use("/calculator", calculatorRoutes);
-
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
