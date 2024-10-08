fe22b241250eafcfdff57af011aea71881cab974
AidanH567 <herstikaidan@gmail.com>
2024-10-08 19:23:37 +1100

    edits

--- Module_5/Routes/calculatorRoutes.js
+++ Module_5/Routes/calculatorRoutes.js
@@ -8,33 +8,15 @@ router.get("/add", (req, res) => {
 });
 
 router.get("/subtract", (req, res) => {
-  let number1 = parseInt(req.query.num1);
-  let number2 = parseInt(req.query.num2);
-  let difference = number1 - number2;
-
-  console.log(difference);
-  res.status(200).json({ result: difference });
+  calculatorController.subtractNumbers(req, res);
 });
 
 router.get("/divide", (req, res) => {
-  let number1 = parseInt(req.query.num1);
-  let number2 = parseInt(req.query.num2);
-  if (number2 === 0) {
-    return res.status(400).json({ error: "Cannot divide by zero." });
-  }
-  let result = number1 / number2;
-
-  console.log(result);
-  res.status(200).json({ result: result });
+  calculatorController.divideNumbers(req, res);
 });
 
 router.get("/multiply", (req, res) => {
-  let number1 = parseInt(req.query.num1);
-  let number2 = parseInt(req.query.num2);
-  let product = number1 * number2;
-
-  console.log(product);
-  res.status(200).json({ result: product });
+  calculatorController.multiplyNumbers(req, res);
 });
 
 module.exports = router;
--- Module_5/controllers/calculatorController.js
+++ Module_5/controllers/calculatorController.js
@@ -6,6 +6,37 @@ const addNumbers = (req, res) => {
   res.status(200);
   res.json({ result: sum });
 };
+const subtractNumbers = (req, res) => {
+  let number1 = parseInt(req.query.num1);
+  let number2 = parseInt(req.query.num2);
+  let difference = number1 - number2;
+
+  console.log(difference);
+  res.status(200).json({ result: difference });
+};
+const divideNumbers = (req, res) => {
+  let number1 = parseInt(req.query.num1);
+  let number2 = parseInt(req.query.num2);
+  if (number2 === 0) {
+    return res.status(400).json({ error: "Cannot divide by zero." });
+  }
+  let result = number1 / number2;
+
+  console.log(result);
+  res.status(200).json({ result: result });
+};
+const multiplyNumbers = (req, res) => {
+  let number1 = parseInt(req.query.num1);
+  let number2 = parseInt(req.query.num2);
+  let product = number1 * number2;
+
+  console.log(product);
+  res.status(200).json({ result: product });
+};
+
 module.exports = {
   addNumbers,
+  subtractNumbers,
+  divideNumbers,
+  multiplyNumbers,
 };
