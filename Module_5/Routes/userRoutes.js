e0b4f9bd5286df55538107172724fb3823355689
AidanH567 <herstikaidan@gmail.com>
2024-10-08 16:41:19 +1100

    edits

--- Module_5/Routes/userRoutes.js
+++ Module_5/Routes/userRoutes.js
@@ -1,6 +1,6 @@
+// Add to new file - routes/userRoutes.js
 const express = require("express");
 const router = express.Router();
-
 const users = [
   { id: 1, name: "Anthony Albanese", country: "AU" },
   { id: 2, name: "Joe Biden", country: "US" },
@@ -8,30 +8,36 @@ const users = [
   { id: 4, name: "Lee Hsien Loong", country: "SG" },
 ];
 
-router.get("/headers", (req, res) => {
-  res.json(req.headers);
+// a POST request with data sent in the body of the request, representing a new user
+router.post("/", (req, res) => {
+  let newUser = req.body; // first update index.js
+  console.log(newUser);
+  // we can add some validation as well
+  if (!newUser.name || !newUser.location) {
+    res.status(500).json({ error: "User must contain a name and location" });
+    return;
+  } else if (!newUser.id) {
+    // if no ID, generate one
+    newUser.id = users.length + 1;
+  }
+  // if the new user is valid, add them to the list
+  users.push(newUser);
+  res.status(200).json(newUser); // return the new user
 });
 
+// get information about this request from the headers
+router.get("/headers", (req, res) => {
+  console.log(req.headers);
+  res.json(req.headers);
+});
+// Dynamic request param endpoint - get the user matching the specific ID ie. /users/3
 router.get("/:id", (req, res) => {
-  let userId = req.params.id;
+  console.log(req.params);
+
+  let userId = req.params.id; // 'id' will be a value matching anything after the / in the request path
   let user = users.find((user) => user.id == userId);
   user
     ? res.status(200).json({ result: user })
     : res.status(404).json({ result: `User ${userId} not found` });
 });
-
-router.post("/", (req, res) => {
-  let newUser = req.body;
-
-  if (!newUser.name || !newUser.country) {
-    res.status(500).json({ error: "User must contain a name and country" });
-    return;
-  } else if (!newUser.id) {
-    newUser.id = users.length + 1;
-  }
-
-  users.push(newUser);
-  res.status(200).json(newUser);
-});
-
 module.exports = router;
--- Module_5/index.js
+++ Module_5/index.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const app = express();
 const port = 3000;
-
 const testRoutes = require("./Routes/testRoutes");
 const calculatorRoutes = require("./Routes/calculatorRoutes");
+const userRoutes = require("./Routes/userRoutes");
 
-app.use("/", express.static("public"));
+app.use("/", express.static("public")); // Serve static files
+app.use(express.json()); // Middleware to parse JSON requests
 
+app.use("/users", userRoutes);
 app.use("/mytest", testRoutes);
 app.use("/calculator", calculatorRoutes);
 
@@ -15,22 +17,14 @@ app.listen(port, () => {
 });
 
 // Second Server
-
 const app1 = express();
 const port1 = 2000;
 
+app1.use("/", express.static("public")); // Serve static files for the second app
 app1.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-app1.use("/", express.static("public"));
-
-app.listen(port1, () => {
-  console.log(`Example app listening
-at http://localhost:${port1}`);
+app1.listen(port1, () => {
+  console.log(`Example app listening at http://localhost:${port1}`);
 });
-
-// import all user routes (up top in index.js)
-const userRoutes = require("./Routes/userRoutes");
-// map the user routes to our app
-app.use("/users", userRoutes);
