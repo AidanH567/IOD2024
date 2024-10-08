bb5d55540b1ed2654ac7b0f39ffb7f7a495da2a2
AidanH567 <herstikaidan@gmail.com>
2024-10-08 14:28:09 +1100

    edits

--- Practice_Folder/index.js
+++ Practice_Folder/index.js
@@ -2,23 +2,21 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-const testRoutes = require("./Routes/testRoutes");
-const calculatorRoutes = require("./Routes/calculatorRoutes");
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
 app.use("/", express.static("public"));
 
-app.use("/mytest", testRoutes);
-app.use("/calculator", calculatorRoutes);
-
 app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
+  console.log(`Example app listening
+at http://localhost:${port}`);
 });
-
-// Second Server
 
 const app1 = express();
 const port1 = 2000;
 
+// Second App
 app1.get("/", (req, res) => {
   res.send("Hello World!");
 });
